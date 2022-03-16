9cbd5e060f1d4a12ef341d0ffa97e20746e8e24b
Your Name <gabriela.czerniawska@gmail.com>
2022-03-16 19:48:06 +0100

    created service db

---
+++ src/main/java/com/qa/horses/service/HorsesServiceDB.java
@@ -0,0 +1,63 @@
+package com.qa.horses.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.qa.horses.domain.Horses;
+import com.qa.horses.repo.HorsesRepo;
+
+@Service
+public class HorsesServiceDB implements HorsesInterface{
+	
+	private HorsesRepo repo;
+	
+	public HorsesServiceDB(HorsesRepo repo) {
+		super();
+		this.repo = repo;
+	}
+
+	@Override
+	public Horses create(Horses x) {
+		return this.repo.save(x);
+	}
+
+	@Override
+	public List<Horses> readAll() {
+		return this.repo.findAll();
+	}
+
+	@Override
+	public Horses readById(Long id) {
+		Optional<Horses> optionalRead = this.repo.findById(id);
+		return optionalRead.orElse(null); //add custom return message
+	}
+
+	@Override
+	public Horses update(Long id, Horses y) {
+		Optional<Horses> optionalHorse = this.repo.findById(id);
+		Horses found = optionalHorse.get();
+		found.setName(y.getName());
+		found.setBreed(y.getBreed());
+		found.setAge(y.getAge());
+		found.setGender(y.getGender());
+		return this.repo.save(found);
+	}
+
+	@Override
+	public Boolean delete(Long id) {
+		this.repo.deleteById(id);
+		return !this.repo.existsById(id);
+	}
+	
+	// --alternative delete method
+	
+//	@Override
+//	public Horses delete(Long id) {
+//		Optional<Horses> delete = this.repo.findById(id);
+//		this.repo.deleteById(id);
+//		return delete.orElse(null);
+//	}
+
+}
